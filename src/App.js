c729105c94c3bbfff841fc86a817905805c48792
anthonycoded <barbels33@gmail.com>
2021-03-30 14:56:23 -0400

    added stripe to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 import "./app.css";
 import Admin from "./components/Admin";
 import Header from "./components/UI/Header";
 import LandingPage from "./components/LandingPage";
+import Checkout from "./components/Checkout/Checkout";
 import Login from "./components/Authentication/Login";
 import Register from "./components/Authentication/Register";
-
+import Success from "./components/Checkout/Success";
+const stripePromise = loadStripe(process.env.STRIPE_PRIVATE_KEY);
 
 const App = () => {
   return (
@@ -21,6 +25,11 @@ const App = () => {
           <Route path="/admin" exact component={Admin}></Route>
           <Route path="/login" component={Login}></Route>
           <Route path="/register" component={Register}></Route>
+          <Route path="/checkout">
+            <Elements stripe={stripePromise}>
+              <Checkout selectedTrack={selectedTrack} />
+            </Elements>
+          </Route>
         </Switch>
       </Router>
     </div>
